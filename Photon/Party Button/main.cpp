4b8e94ead365cfb1531dd4ff0ccc208970b0a77c
Jake Tobin <jake@tobin.io>
2015-10-11 00:34:10 -0400

    Stuff yo

--- Photon/Party Button/main.cpp
+++ Photon/Party Button/main.cpp
@@ -4,7 +4,7 @@
 #include "InternetButton/InternetButton.h"
 
 /* Here's a nice combination of features that I like to use.
-Note the use of the allButtons function. */
+ Note the use of the allButtons function. */
 
 InternetButton b = InternetButton();
 
@@ -18,69 +18,72 @@ void setup() {
 }
 
 void loop(){
-
+    
+    
     // If this calls for a full spectrum situation, let's go rainbow!
     if(b.allButtonsOn()) {
         // Publish the event "allbuttons" for other services like IFTTT to use
-        Spark.publish("allbuttons",NULL, 60, PRIVATE);
+        Spark.publish("allbuttons","allbuttons", 60, PRIVATE);
         b.rainbow(5);
         rainbow_mode = true;
-
+        
         // If all buttons are on, don't try to process
         // the individual button responses below.  Just return.
         return;
     }
-
+    
     // If we are not in rainbow mode anymore, turn the LEDs off
     if (rainbow_mode == true) {
         b.allLedsOff();
         rainbow_mode = false;
     }
-
+    
     // Process individual buttons and LED response
     if (b.buttonOn(1)) {
         b.ledOn(12, 255, 0, 0); // Red
+        b.rainbow(5);
+        rainbow_mode = true;
         // Publish the event "button1" for other services like IFTTT to use
-        Spark.publish("button1",NULL, 60, PRIVATE);
+        Spark.publish("button1","button1", 60, PRIVATE);
         delay(500);
     }
     else {
         b.ledOn(12, 0, 0, 0);
     }
-
+    
     if (b.buttonOn(2)) {
         b.ledOn(3, 0, 255, 0); // Green
         // Publish the event "button2" for other services like IFTTT to use
-        Spark.publish("button2",NULL, 60, PRIVATE);
+        Spark.publish("button2","button2", 60, PRIVATE);
         delay(500);
     }
     else {
         b.ledOn(3, 0, 0, 0);
     }
-
+    
     if (b.buttonOn(3)) {
         b.ledOn(6, 0, 0, 255); // Blue
         // Publish the event "button3" for other services like IFTTT to use
-        Spark.publish("button3",NULL, 60, PRIVATE);
+        Spark.publish("button3","button3", 60, PRIVATE);
         delay(500);
     }
     else {
         b.ledOn(6, 0, 0, 0);
     }
-
+    
     if (b.buttonOn(4)) {
         b.ledOn(9, 255, 0, 255); // Magenta
         // Publish the event "button4" for other services like IFTTT to use
-        Spark.publish("button4",NULL, 60, PRIVATE);
+        Spark.publish("button4","button4", 60, PRIVATE);
         delay(500);
     }
     else {
         b.ledOn(9, 0, 0, 0);
     }
-
+    
     if(b.allButtonsOff()) {
         // Do something here when all buttons are off
-         //b.allLedsOn( 0, 255, 0);
-
+        //b.allLedsOn( 0, 255, 0);
+        
     }
 }
--- Pi/flask me/ButtonServer.py
+++ Pi/flask me/ButtonServer.py
@@ -11,12 +11,33 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/add', methods=['POST'])
+@app.route('/button1', methods=['POST'])
 def add_entry():
     app.logger.warning('A warning occurred (%d apples)', 42)
     app.logger.error('An error occurred')
     app.logger.info('Info')
-    return "foo"
+    return "1"
+
+@app.route('/button2', methods=['POST'])
+def add_entry2():
+    app.logger.warning('A warning occurred (%d apples)', 42)
+    app.logger.error('An error occurred')
+    app.logger.info('Info')
+    return "2"
+
+@app.route('/button3', methods=['POST'])
+def add_entry3():
+    app.logger.warning('A warning occurred (%d apples)', 42)
+    app.logger.error('An error occurred')
+    app.logger.info('Info')
+    return "3"
+
+@app.route('/button4', methods=['POST'])
+def add_entry4():
+    app.logger.warning('A warning occurred (%d apples)', 42)
+    app.logger.error('An error occurred')
+    app.logger.info('Info')
+    return "4"
 
 
 if __name__ == '__main__':
